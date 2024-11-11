f4bc96eae2c88959a6bffc3ee990c7a9276d0276
Fabiana Pereira <fabianaduarte980@gmail.com>
2024-11-11 02:24:59 -0300

    fix deploy to github pages

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import HomePage from './pages/HomePage.vue'
 import PostsPage from './pages/PostsPage.vue'
@@ -11,6 +11,6 @@ const routes = [
 ]
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
